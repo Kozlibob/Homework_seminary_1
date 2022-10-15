c030f73f6e29ed9b5b1d1705c10dea80f9e0820f
Nicholas Nikulin <kozlibob@gmail.com>
2022-10-15 13:50:00 +0300

    First Homework is done

---
+++ HomeWork1/Homework1/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Введите первое число ");
+double a = double.Parse(Console.ReadLine());
+Console.WriteLine("Введите второе число ");
+double b = double.Parse(Console.ReadLine());
+if (a > b)
+{
+    Console.WriteLine("MAX - " + a);
+    Console.WriteLine("MIN - " + b);
+}
+else if (b > a)
+{
+    Console.WriteLine("MAX - " + b);
+    Console.WriteLine("MIN - " + a);
+}
