162f62a229084aa8bef779162e70f389d90d1816
Nicholas Nikulin <kozlibob@gmail.com>
2022-10-15 14:02:04 +0300

    Second homework is done

---
+++ HomeWork2/HomeWork2/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Введите первое число ");
+double a= double.Parse(Console.ReadLine());
+Console.WriteLine("Введите второе число ");
+double b= double.Parse(Console.ReadLine());
+Console.WriteLine("Введите третье число ");
+double c= double.Parse(Console.ReadLine());
+if (a>b && a>c)
+{
+    Console.WriteLine("MAX - " + a);
+}
+else if (b>a && b>c)
+{
+    Console.WriteLine("MAX - " + b);
+}
+else Console.WriteLine("MAX - " + c);
