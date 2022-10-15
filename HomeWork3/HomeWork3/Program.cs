dc6a068580942b8ed16ed7fe2bb8d39f31d9bba4
Nicholas Nikulin <kozlibob@gmail.com>
2022-10-15 14:21:27 +0300

    Third HomeWork is done

---
+++ HomeWork3/HomeWork3/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите первое число");
+int a= int.Parse(Console.ReadLine());
+if ((a % 2) == 0) 
+{
+    Console.WriteLine("Введенное число четное");
+    Console.ReadLine();
+}
+else
+{
+    Console.WriteLine("Введенное число не четное");
+    Console.ReadLine();
+}
