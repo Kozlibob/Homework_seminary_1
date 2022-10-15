d3ddebe22ff9f5aa34f703f5f384144dd8b28b4d
Nicholas Nikulin <kozlibob@gmail.com>
2022-10-15 16:29:37 +0300

    fourth HomeWork4 is done

---
+++ HomeWork4/HomeWork4/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Write("Введите число - ");
+int a = int.Parse(Console.ReadLine());
+for(int j = 1; j <= a; j++)
+{
+    if ((j % 2) == 0)
+    {
+        Console.WriteLine("Четные числа до Вашего " + j);
+    }
+}
